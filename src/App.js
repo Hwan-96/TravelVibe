e00e1d3eb34bb245ff19763971a6b6e84657a7f9
Hwan-96 <yunhwan0960@gmail.com>
2024-06-12 14:22:09 +0900

    레이아웃 설계

--- src/App.js
+++ src/App.js
@@ -12,24 +12,31 @@ import Channel from './pages/Channel'
 import Video from './pages/Video'
 import Search from './pages/Search'
 import ErrorPage from './pages/ErrorPage'
+import Header from './components/section/Header'
+import Main from './components/section/Main'
+import Footer from './components/section/Footer'
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/today' element={<Today/>} />
-        <Route path='/traveler' element={<Traveler/>} />
-        <Route path='/koreatrip' element={<Koreatrip/>} />
-        <Route path='/europetrip' element={<Europetrip/>} />
-        <Route path='/americatrip' element={<Americatrip/>} />
-        <Route path='/asiatrip' element={<Asiatrip/>} />
-        <Route path='/afreecatrip' element={<Afreecatrip/>} />
-        <Route path='/channel/:channelID' element={<Channel/>} />
-        <Route path='/video/:videoID' element={<Video/>} />
-        <Route path='/search/:searchID' element={<Search/>} />
-        <Route path='/*' element={<ErrorPage/>} />
-      </Routes>
+      <Header/>
+      <Main>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/today' element={<Today/>} />
+          <Route path='/traveler' element={<Traveler/>} />
+          <Route path='/koreatrip' element={<Koreatrip/>} />
+          <Route path='/europetrip' element={<Europetrip/>} />
+          <Route path='/americatrip' element={<Americatrip/>} />
+          <Route path='/asiatrip' element={<Asiatrip/>} />
+          <Route path='/afreecatrip' element={<Afreecatrip/>} />
+          <Route path='/channel/:channelID' element={<Channel/>} />
+          <Route path='/video/:videoID' element={<Video/>} />
+          <Route path='/search/:searchID' element={<Search/>} />
+          <Route path='/*' element={<ErrorPage/>} />
+        </Routes>
+      </Main>
+      <Footer/>
     </BrowserRouter>
   )
 }
