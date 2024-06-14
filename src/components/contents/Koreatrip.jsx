c40c7aec40eef3dd0a012ab3ca992503e5f72c34
Hwan-96 <yunhwan0960@gmail.com>
2024-06-14 16:24:43 +0900

    240614 임시커밋

--- src/components/contents/Koreatrip.jsx
+++ src/components/contents/Koreatrip.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { koreaTripText } from '../../data/koreatrip'
+import { Link } from 'react-router-dom'
 
 const Koreatrip = () => {
   return (
-    <div>Koreatrip</div>
+    <section id='koreaTrip'>
+      <h2>국내 여행</h2>
+      <div className="video-inner">
+        {koreaTripText.map((video, key)=>(
+          <div className="video" key={key}>
+            <div className="video-thumb play-icon">
+              <Link to={`/video/${video.videoId}`}>
+                <img src={video.img} alt={video.title} />
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
 
