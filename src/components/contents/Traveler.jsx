bcecb5f71f992254d7a7eefb3428d0b269ccbcc4
Hwan-96 <yunhwan0960@gmail.com>
2024-06-14 14:44:49 +0900

    추천영상, 추천여행자

--- src/components/contents/Traveler.jsx
+++ src/components/contents/Traveler.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import {travelerText} from '../../data/traveler'
+import { Link } from 'react-router-dom'
 
 const Traveler = () => {
   return (
-    <div>Traveler</div>
+    <section id='traveler'>
+      <h2>추천 여행자</h2>
+      <div className='traveler-inner overflow'>
+        {travelerText.map((traveler, key) => (
+          <div className="traveler" key={key}>
+            <div className="traveler-img play-icon">
+              <Link to={`/channel/${traveler.channelId}`}>
+                <img src={traveler.img} alt={traveler.name} />
+              </Link>
+            </div>
+            <div className="traveler-info">
+              <Link to={`/channel/${traveler.channelId}`}>
+                {traveler.name}
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
 
--- src/pages/Traveler.jsx
+++ src/pages/Traveler.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
 import Main from '../components/section/Main'
+import {travelerText} from '../data/traveler'
+import { Link } from 'react-router-dom'
 
 const Traveler = () => {
   return (
     <Main
       title="추천 여행자"
-      description="오늘의 추천 여행자입니다."
-    >
-      Traveler
+      description="오늘의 추천 여행자입니다.">
+
+        <section id='travelerPage'>
+          <h2>추천 여행자</h2>
+          <div className='traveler-inner'>
+            {travelerText.map((traveler, key) => (
+              <div className="traveler" key={key}>
+                <div className="traveler-img play-icon">
+                  <Link to={`/channel/${traveler.channelId}`}>
+                    <img src={traveler.img} alt={traveler.name} />
+                  </Link>
+                </div>
+                <div className="traveler-info">
+                  <Link to={`/channel/${traveler.channelId}`}>
+                    {traveler.name}
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </div>
+        </section>
+
     </Main>
   )
 }
